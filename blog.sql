1dda627efcfc2a39300661827cc847ce710dedfa
Mdidu <56021893+Mdidu@users.noreply.github.com>
2020-02-23 18:33:15 +0100

    Add files via upload
    commit database

---
+++ blog.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 23 fév. 2020 à 17:33
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE IF NOT EXISTS `articles` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `contend` longtext NOT NULL,
+  `date` int(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `contend`, `date`, `user_id`) VALUES
+(1, 'First', 'First', 1582207626, 1),
+(2, 'tt', 'tt update', 1582214321, 1),
+(3, 'new', 'new article', 1582306865, 2),
+(4, 'aa update', 'Un test', 1582360650, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentary`
+--
+
+DROP TABLE IF EXISTS `commentary`;
+CREATE TABLE IF NOT EXISTS `commentary` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `contend` longtext NOT NULL,
+  `date` int(20) NOT NULL,
+  `articles_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_articles_id` (`articles_id`),
+  KEY `fk_user_id_commentary` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `commentary`
+--
+
+INSERT INTO `commentary` (`id`, `contend`, `date`, `articles_id`, `user_id`) VALUES
+(1, 'First', 1582207637, 1, 2),
+(2, 'test', 1582207831, 1, 2),
+(3, 'test', 1582306494, 2, 1),
+(4, 'bof', 1582306854, 1, 1),
+(5, 'test add comment', 1582366725, 3, 2),
+(6, 'Un commentaire pour le nouveau insert', 1582381824, 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groupe`
+--
+
+DROP TABLE IF EXISTS `groupe`;
+CREATE TABLE IF NOT EXISTS `groupe` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `rank` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `groupe`
+--
+
+INSERT INTO `groupe` (`id`, `rank`) VALUES
+(1, 'Users'),
+(2, 'Moderator'),
+(3, 'Administrator');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(255) NOT NULL,
+  `password` text NOT NULL,
+  `group_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_group_id` (`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `pseudo`, `password`, `group_id`) VALUES
+(1, 'Mdidu', '$2y$10$aPD/M2Be311RS3.xOPXO0.AjMtopgPcSeg3xvl.4FtIkxvd7NDnCC', 3),
+(2, 'goulum', '$2y$10$4FxCSDY.3b/XTQiTEA4ygu.RI7fZZLZ16Qf3EXdoc7Uf3pQQAiYHW', 1);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `articles`
+--
+ALTER TABLE `articles`
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `commentary`
+--
+ALTER TABLE `commentary`
+  ADD CONSTRAINT `fk_articles_id` FOREIGN KEY (`articles_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user_id_commentary` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_group_id` FOREIGN KEY (`group_id`) REFERENCES `groupe` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
